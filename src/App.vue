3189a9a05d1ae200e2837c7f2d7e0aeec07d80be
zhoushaodong666 <1138894663@qq.com>
2019-07-08 11:23:21 +0800

    结构修改

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <!--<img src="./assets/logo.png">-->
+
+      <!-- 显示的是当前路由地址所对应的内容 -->
     <router-view/>
   </div>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/pages/home/Home'
+import List from '@/pages/list/List'
+
 
 Vue.use(Router)
 
@@ -8,8 +10,12 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+      name: 'Home',
+      component: Home
+    },{
+      path: '/list',
+      name: 'List',
+      component: List
+    },
   ]
 })
