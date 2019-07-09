fa56fa81776b6b3630763780652677ad674f7a4b
zhoushaodong666 <1138894663@qq.com>
2019-07-09 17:52:08 +0800

    页面头部

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,6 +1,8 @@
 <template>
-    <div>
-        this is header
+    <div class="header">
+        <div class="header-left">返回</div>
+        <div class="header-input">请输入城市/景点</div>
+        <div class="header-right">城市</div>
     </div>
 </template>
 
@@ -10,6 +12,27 @@
 	}
 </script>
 
-<style scoped>
+<style lang="stylus" scoped>
+    .header
+        display flex
+        line-height : .86rem
+        background #00bcd4
+        color: #fff
+        .head-left
+            width: .64rem
+            float:left
+        .header-input
+            flex: 1
+            height: .64rem
+            line-height :.64rem
+            margin-top : .12rem
+            margin-left : .2rem
+            background :#fff
+            border-radius: .1rem
+            color: #ccc
+        .head-right
+            width:1.24rem
+            float :right
+            text-align :center
 
 </style>
