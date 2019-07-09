1e81ad1e2d6ed1ce5724e5de46684a24d100c034
zhoushaodong666 <1138894663@qq.com>
2019-07-09 18:29:26 +0800

    页面头部

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,8 +1,15 @@
 <template>
     <div class="header">
-        <div class="header-left">返回</div>
-        <div class="header-input">请输入城市/景点</div>
-        <div class="header-right">城市</div>
+        <div class="header-left">
+            <div class="iconfont back-icon">&#xe624;</div>
+        </div>
+        <div class="header-input">
+            <span class="iconfont">&#xe632;</span>
+            请输入城市/景点
+        </div>
+        <div class="header-right">城市
+            <span class="iconfont arrow-icon">&#xe64a;</span>
+        </div>
     </div>
 </template>
 
@@ -18,21 +25,29 @@
         line-height : .86rem
         background #00bcd4
         color: #fff
-        .head-left
+        .header-left
             width: .64rem
             float:left
+            .back-icon
+                text-align :center
+                font-size :.4rem
+
         .header-input
             flex: 1
             height: .64rem
             line-height :.64rem
             margin-top : .12rem
-            margin-left : .2rem
+            margin-left : .12rem
+            padding-left : .2rem
             background :#fff
             border-radius: .1rem
             color: #ccc
-        .head-right
+        .header-right
             width:1.24rem
             float :right
-            text-align :center
+            text-align: center
+            .arrow-icon
+                margin-left : -.04rem
+                font-size : .24rem
 
 </style>
