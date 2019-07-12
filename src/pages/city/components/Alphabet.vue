cf4f59e03e43660403b6f39fd7184597626227b5
zhoushaodong666 <1138894663@qq.com>
2019-07-12 17:42:57 +0800

    add city ajax

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,31 @@
+<template>
+    <ul class="list">
+        <li class="item" v-for="(item,key) of cities" :key="key">{{key}}</li>
+    </ul>
+</template>
+
+<script>
+	export default {
+		name: "CityAlphabet",
+        props:{
+			cities:Object
+        }
+	}
+</script>
+
+<style lang="stylus" scoped>
+    @import '~styles/varibles.styl'
+    .list
+        display :flex
+        flex-direction :column
+        justify-content :center
+        position: absolute
+        right: 0
+        top: 1.58rem
+        bottom: 0
+        width: .4rem
+        .item
+            line-height: .4rem
+            text-align: center
+            color:$bgColor
+</style>
