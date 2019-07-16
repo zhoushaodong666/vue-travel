18fad4372e156191e10293edc477675413c97fb3
zhoushaodong666 <1138894663@qq.com>
2019-07-16 11:12:28 +0800

    banner模块完成

--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -1,17 +1,42 @@
 <template>
-    <div class="banner">
-        <img class="banner-img"
-             src="//img1.qunarzz.com/sight/p0/1709/41/411f234d79457081a3.img.jpg_600x330_b5e86902.jpg">
-        <div class="banner-info">
-            <div class="banner-title">长隆野生动物世界(AAAAA景区)</div>
-            <div class="banner-number"><span class="iconfont banner-icon">&#xe692;</span>39</div>
+    <div>
+        <div class="banner" @click="handleBannerClick">
+            <img class="banner-img"
+                 src="//img1.qunarzz.com/sight/p0/1709/41/411f234d79457081a3.img.jpg_600x330_b5e86902.jpg">
+            <div class="banner-info">
+                <div class="banner-title">长隆野生动物世界(AAAAA景区)</div>
+                <div class="banner-number"><span class="iconfont banner-icon">&#xe692;</span>39</div>
+            </div>
         </div>
+        <common-gallary :imgs="imgs" v-show="showGallary" @close="handleGallaryClose"></common-gallary>
     </div>
 </template>
 
 <script>
+	import CommonGallary from 'common/gallary/Gallary'
+
 	export default {
-		name: "DetailBanner"
+		name: "DetailBanner",
+        data (){
+		    return {
+			    showGallary:false,
+			    imgs:[
+				    'http://img1.qunarzz.com/sight/p0/1709/41/411f234d79457081a3.img.jpg_r_800x800_ea3a0585.jpg',
+				    'http://img1.qunarzz.com/sight/p0/1412/66/cd09ac9548221dcc4cef8dde5913c780.water.jpg_r_800x800_bea4d4a4.jpg'
+			    ]
+		    }
+        },
+        methods:{
+	        handleBannerClick(){
+                this.showGallary =true
+            },
+	        handleGallaryClose(){
+		        this.showGallary =false
+            }
+        },
+		components: {
+			CommonGallary
+		}
 	}
 </script>
 
